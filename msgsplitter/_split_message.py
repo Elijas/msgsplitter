57d97d9b60659b78c06d28ea3006dbf71330cf9f
Elijas Dapšauskas <master.elijas@gmail.com>
2021-03-26 23:19:17 +0200

    Fix module encapsulation

--- msgsplitter/_split_message.py
+++ msgsplitter/_split_message.py
@@ -4,19 +4,19 @@ from msgsplitter.exceptions import LengthLimitTooLowError
 from msgsplitter.formatters.formatter_base import FormatterBase
 
 
-def get_this_message_length(i, j, chunk_lengths):
+def _get_this_message_length(i, j, chunk_lengths):
     text_character_count = sum(chunk_lengths[i:j + 1])
     space_character_count = j - i
     return text_character_count + space_character_count
 
 
-def get_groups(chunk_count, chunk_lengths, formatter):
+def _get_groups(chunk_count, chunk_lengths, formatter):
     groups = []
     i = 0
     while i < chunk_count:
         last_j = None
         for j in range(i, chunk_count):
-            message_length = get_this_message_length(i, j, chunk_lengths)
+            message_length = _get_this_message_length(i, j, chunk_lengths)
             if formatter.is_message_too_long(message_length, len(groups) + 1):
                 if last_j is None:
                     raise LengthLimitTooLowError
@@ -43,4 +43,4 @@ def split_message(message: str, formatter: FormatterBase) -> List[str]:
     chunk_lengths = list(map(len, chunks))
     chunk_count = len(chunk_lengths)
 
-    return [' '.join(chunks[i:j + 1]) for i, j in get_groups(chunk_count, chunk_lengths, formatter)]
+    return [' '.join(chunks[i:j + 1]) for i, j in _get_groups(chunk_count, chunk_lengths, formatter)]
